84f8b7f2e9274a0197cb97c5696ad3bc1f73086f
Matthew Viacrusis <matthewvia77@gmail.com>
2025-03-04 00:04:01 +0800

    tbh i forgot what exactly I edited but its something to do with deployment

--- portfolio/next.config.ts
+++ portfolio/next.config.ts
@@ -4,9 +4,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: '',
-  // Only add this if you're using a custom domain instead of *.github.io
-  // assetPrefix: '',
 }
 
 module.exports = nextConfig
