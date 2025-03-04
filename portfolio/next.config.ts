4bd6047daaa98c5d1b35c12e26b3d84f3584af80
Matthew Viacrusis <matthewvia77@gmail.com>
2025-03-04 10:13:03 +0800

    huhuu

--- portfolio/next.config.ts
+++ portfolio/next.config.ts
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // Ensures static HTML export
-  distDir: 'out',   // Ensures the build goes to the correct directory
-  images: {
-    unoptimized: true, // Avoids Image Optimization issues on GitHub Pages
-  },
 };
 
 module.exports = nextConfig;
